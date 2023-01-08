c874e9d3cd98be41f3a5f5cd1f724e42dbb2986c
wallenben <cookietime01@gmail.com>
2023-01-08 01:29:55 -0600

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://wallenben.github.io',
-	base: '/site',
+	base: '/',
+	integrations: [mdx(), sitemap(), tailwind()],
 });
