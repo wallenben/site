47b74996ee7b800d584aa26432821f588b73ccad
wallenben <cookietime01@gmail.com>
2023-01-08 00:13:09 -0600

    e

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+	site: 'https://wallenben.github.io',
+	base: '/site',
 });
